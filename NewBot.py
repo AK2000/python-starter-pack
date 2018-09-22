2019833b892a3d61d402b9c7a90d1652fcaa340e
Alok Kamatar <alok.kamatar@outlook.com>
2018-09-22 00:05:36 -0500

    Added newBot python with limited functionallity

---
+++ NewBot.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 21 22:38:37 2018
+
+@author: alokk
+"""
+# keep these three import statements
+import game_API
+import fileinput
+import json
+
+# your import statements here
+import random
+
+first_line = True # DO NOT REMOVE
+
+# global variables or other functions can go here
+stances = ["Rock", "Paper", "Scissors"]
+nodeCircle = [0,1,3,1]
+nodeCircle2 = [0,6,0,10]
+turnCounter = 0
+
+def get_winning_stance(stance):
+    if stance == "Rock":
+        return "Paper"
+    elif stance == "Paper":
+        return "Scissors"
+    elif stance == "Scissors":
+        return "Rock"
+
+def get_third_stance(stance1, stance2):
+    if (stance1 == "Rock" and stance2 == "Paper") or (stance2 == "Rock" and stance1 == "Paper"):
+        return "Scissors"
+    if (stance1 == "Rock" and stance2 == "Scissors") or (stance2 == "Rock" and stance1 == "Scissors"):
+        return "Paper"
+    if (stance1 == "Scissors" and stance2 == "Paper") or (stance2 == "Scissors" and stance1 == "Paper"):
+        return "Rock"
+    
+# main player script logic
+# DO NOT CHANGE BELOW ----------------------------
+for line in fileinput.input():
+    if first_line:
+        game = game_API.Game(json.loads(line))
+        first_line = False
+        continue
+    game.update(json.loads(line))
+# DO NOT CHANGE ABOVE ---------------------------
+
+    # code in this block will be executed each turn of the game
+
+    me = game.get_self()
+    turnCounter = turnCounter + 1  
+    
+    if me.location == me.destination: # check if we have moved this turn
+        if game.has_monster(me.location):
+            destination_node = me.destination
+        else:
+            destination_node = turnCounter%len(nodeCircle)
+    else:
+        destination_node = me.destination
+
+    if game.has_monster(me.location):
+        # if there's a monster at my location, choose the stance that damages that monster
+        if(game.get_opponent().stance == me.location):
+            
+        else:
+            chosen_stance = get_winning_stance(game.get_monster(me.location).stance)
+    else:
+        # otherwise, pick a random stance
+        chosen_stance = stances[random.randint(0, 2)]
+
+    # submit your decision for the turn (This function should be called exactly once per turn)
+    game.submit_decision(destination_node, chosen_stance)
+
+def get_back(node1, node2, numTurns):
+    paths = game.shortest_paths(node1, node2)
+    minNumTurns = len(paths[0]) * (7-me.speed)
+    return numTurns <= minNumTurns
+    
